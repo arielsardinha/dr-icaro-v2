bae3d9c095ae8b7d90a5154f9a8a24bc6e71e451
arielsardinha <odararepresentacoes@outlook.com.br>
2021-11-11 08:55:33 -0200

    corrigindo texto do icaro

--- src/ui/partials/sobre-nos/drIcaro.tsx
+++ src/ui/partials/sobre-nos/drIcaro.tsx
@@ -48,7 +48,8 @@ export const DrIcaro = () => (
           acompanhamento dos primeiros dias em Homecare, para quem mora em
           Palmas, e no Hotel para quem vier operar de outra cidade.
         </p>
-        <p style={{ textAlign: "center" }}>Dream Sculptor</p>
+        <p >Dream Sculptor</p>
+        <p>Embaixador Total Definer Brasil</p>
         <BoxImages>
           <a
             href="http://www2.cirurgiaplastica.org.br/"
